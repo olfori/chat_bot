5aa62f448bb260fc3c66f8127b8b166d3afa3acc
o.lyubimov <olfori@yandex.ru>
2025-02-22 15:00:33 +0300

    Добавил testing +

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import re
 import datetime
-from random import randint, choice, shuffle
+import random
 from const import const
 from helpers import helpers
 from services import services
 
 class Bot:
-    """Класс Бот с заданными параметрами"""
-    def __init__(self):
+    """
+    Класс Бот с заданными параметрами
+    """
+
+    def __init__(self, testing=False):
+        self.testing = testing
         self.name = const.NAME
         self.age = const.BORN_IN
         self.prev_answer = ''
@@ -15,23 +19,29 @@ class Bot:
         self.phrases = helpers.file_to_list(const.MAIN_FILE)
 
     def check_key(self, question):
-        """Обрабатывает ключевые вопросы"""
+        """
+        Обрабатывает ключевые вопросы
+        """
         return {
             'dt': str(datetime.datetime.fromtimestamp(const.BORN_IN)),
             'name': self.name,
         }.get(question)
 
     def get_shortest_element(self):
-        """Возвращает первый элемент самой короткой строки"""
+        """
+        Возвращает первый элемент самой короткой строки
+        """
         shortest = min(self.phrases, key=len, default='')
         return services.get_first(shortest)
 
     def no_answer(self, question: str):
-        """Обрабатывает ситуацию, когда нет ответа"""
+        """
+        Обрабатывает ситуацию, когда нет ответа
+        """
         elm = self.get_shortest_element()
-        rnd_val = randint(0, 100)
+        rnd_val = random.randint(0, 100)
         if rnd_val > 100:
-            return choice(const.DONT_KNOW_PHRASES)
+            return random.choice(const.DONT_KNOW_PHRASES)
         if rnd_val > 50 and elm:
             return elm
 
@@ -40,12 +50,14 @@ class Bot:
             phrase_0 = services.get_first(phrase)
             line_list = re.findall(r'[^;\n]+', phrase_0.lower())
             if any(s.count(phrase_0) > 0 and len(s) > 1 for s in question_words) and len(line_list) > 1:
-                return choice(line_list[1:])
+                return random.choice(line_list[1:])
         
-        return services.get_first(choice(self.phrases))
+        return services.get_first(random.choice(self.phrases))
 
     def save_new_phrases(self, question):
-        """Сохраняет новые фразы"""
+        """
+        Сохраняет новые фразы
+        """
         prev_answer = self.prev_answer
         if prev_answer == question or any(prev_answer == services.get_first(s) for s in self.phrases):
             return
@@ -53,7 +65,9 @@ class Bot:
         self.phrases.append(question)
 
     def save_prev_answer(self, question):
-        """Сохраняем предыдущ ответ, если вопрос есть в базе"""
+        """
+        Сохраняем предыдущ ответ, если вопрос есть в базе
+        """
         for i, s in enumerate(self.phrases):
             line_list = re.findall(r'[^;\n]+', s)
             if self.prev_answer == line_list[0] and self.prev_answer:
@@ -61,7 +75,9 @@ class Bot:
                 break
 
     def find_answer(self, question):
-        """Поиск ответа в базе"""
+        """
+        Поиск ответа в базе
+        """
         min_letters = 6  # Найдет ответ только если более 6 букв совпало
         res = { "max": min_letters, "answers": [] } 
         for i, s in enumerate(self.phrases):
@@ -74,13 +90,16 @@ class Bot:
                     if number_of_letters >= res["max"]:
                         res["max"] = number_of_letters 
                         res["answers"] += db_answers
-                    # print(db_question, db_answers, number_of_letters, res["max"])
+                    if self.testing:
+                        print(db_question, db_answers, number_of_letters, res["max"])
         if res["answers"]:
-            return choice(res["answers"])
+            return random.choice(res["answers"])
         return None
 
     def get_answer(self, question):
-        """Находит ответ на заданный вопрос"""
+        """
+        Находит ответ на заданный вопрос
+        """
         self.question = helpers.str_clr(question).lower()
         if self.question == 'ex':
             self.is_on = False
@@ -97,11 +116,7 @@ class Bot:
         self.prev_answer = helpers.str_clr(answer)
         return self.prev_answer
     
-"""
-Что надо сделать?
-- можно расширять ответ, например: хм.., | могу предположить, что | ра
-- попробовать подбирать ответы по числу вхождения: букв, слогов
-"""
+
 def additional():
     """Доп действия: убираю повторы в БД и короткие ответы"""
     from collections import Counter
@@ -111,9 +126,9 @@ def additional():
     for phrase in b.phrases:
         line_list = re.findall(r'[^;\n]+', phrase)
         question = line_list[0]
+        answers = line_list[1:]
         if question:
             questions.append(question)
-        answers = line_list[1:]
         filtered_answers = list(set(answers))
         if len(answers) != len(filtered_answers): # Повторы в ответах
             print(question, Counter(answers))
@@ -135,11 +150,18 @@ def additional():
     # print(filtered_res)
 
 def tests():
-    phrase = 'кто ты такой'
-    print(Bot().find_answer(phrase))
+    phrase = 'кто же ты такой'
+    print(Bot(True).find_answer(phrase))
 
 if __name__=='__main__':
     # additional()
     tests()
 
+"""
+Что надо сделать?
+- можно расширять ответ, например: хм.., | могу предположить, что | ра
+- попробовать подбирать ответы по числу вхождения: букв, слогов
+    + проверил по числу букв: пришлось ограничить минимум
+- сохранять ответ 1 раз, в строке сохранять номер строки с ответом через ';'
+"""
     
